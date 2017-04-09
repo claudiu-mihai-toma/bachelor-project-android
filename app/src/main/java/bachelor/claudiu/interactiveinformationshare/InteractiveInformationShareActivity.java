b6ca179b3f0bf0242e15a5ae357e428cc786f82e
Claudiu Toma <tomaclaudiumihai@gmail.com>
2017-04-09 16:51:09 +0300

    Successfully shared text information to this app. Added a decent icon.

--- app/src/main/java/bachelor/claudiu/interactiveinformationshare/InteractiveInformationShareActivity.java
+++ app/src/main/java/bachelor/claudiu/interactiveinformationshare/InteractiveInformationShareActivity.java
@@ -1,13 +1,33 @@
 package bachelor.claudiu.interactiveinformationshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InteractiveInformationShareActivity extends AppCompatActivity {
+
+    private TextView intentReceivedTextView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interactive_information_share);
+
+        intentReceivedTextView = (TextView)findViewById(R.id.intent_received_text_view);
+        Intent intent = getIntent();
+        if (intent != null) {
+            String textShared = intent.getStringExtra(Intent.EXTRA_TEXT);
+            if (textShared != null)
+            {
+                intentReceivedTextView.setText(textShared);
+            }
+            else
+            {
+                intentReceivedTextView.setText("ERROR! :(");
+                Toast.makeText(this, "Something went wrong!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
