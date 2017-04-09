090f36ced7f756b4f56bc2a389cb15c5b86c531d
Claudiu Toma <tomaclaudiumihai@gmail.com>
2017-04-09 12:57:17 +0300

    Created project.

---
+++ app/src/main/java/bachelor/claudiu/interactiveinformationshare/InteractiveInformationShareActivity.java
@@ -0,0 +1,13 @@
+package bachelor.claudiu.interactiveinformationshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InteractiveInformationShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interactive_information_share);
+    }
+}
